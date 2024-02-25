be996379bd10237366036f89bb05e4f709656029
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2024-02-25 21:43:29 +0800

    setup project structure

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Carousel() {
   return (
-    <section className="bg-center min-w-full rounded-3xl bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+    <section className="bg-center rounded-3xl bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-white md:text-4xl lg:text-5xl">
           IKATAN ALUMNI TEKNIK LINGKUNGAN
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,8 @@
 export default function AdminLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <>
       <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
